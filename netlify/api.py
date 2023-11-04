054ea823ad853cf1f630021785640da798d68748
Disni Jayawickrama <it20147778@my.sliit.lk>
2023-11-05 01:57:13 +0530

    #s

--- netlify/api.py
+++ netlify/api.py
@@ -85,6 +85,7 @@ def message_probability(user_message, recognised_words, single_response=False, r
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
+            
 
     # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
